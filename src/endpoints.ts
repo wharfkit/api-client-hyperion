a02e2ff0d7ce0a883384ec0070cf94a77ddf684b
dafuga <danielfugere28@gmail.com>
2023-09-22 18:40:05 -0700

    chore: handling different API versions

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -5,30 +5,55 @@ import {
     Int64Type,
     NameType,
     PublicKeyType,
+    TimePointType,
+    UInt64Type,
 } from '@wharfkit/antelope'
 import {
-    GetABISnapshotResponse,
-    GetActionsResponse,
-    GetCreatedAccountsResponse,
-    GetCreatorResponse,
-    GetDeltasResponse,
-    GetKeyAccountsResponse,
-    GetLinksResponse,
-    GetProposalsResponse,
-    GetTableStateResponse,
-    GetTokensResponse,
-    GetTransactionResponse,
-    GetVotersResponse,
+    V1,
+    V2,
+    SortType,
 } from './types'
 
-export class HyperionAPIClient {
+export class HyperionAPIClient  {
+    public v1: HyperionV1APIClient;
+    public v2: HyperionV2APIClient;
+
+    constructor(private client: APIClient) {
+        this.v1 = new HyperionV1APIClient(client);
+        this.v2 = new HyperionV2APIClient(client);
+    }
+}
+
+export class HyperionV1APIClient {
+    constructor(private client: APIClient) {}
+    
+    async get_actions(options: {
+        account_name: NameType
+        pos?: UInt64Type;
+        offset?: UInt64Type;
+        filter?: UInt64Type;
+        sort?: SortType;
+        after?: TimePointType;
+        before?: TimePointType;
+        parent?: number;
+    }): Promise<any> {
+        return this.client.call({
+            path: `/v1/history/get_actions`,
+            method: 'POST',
+            // responseType: GetActionsResponse,
+            params: options
+        })
+    }
+}
+
+export class HyperionV2APIClient {
     constructor(private client: APIClient) {}
 
     async get_abi_snapshot(
         contract: string,
         block?: number,
         fetch = false
-    ): Promise<GetABISnapshotResponse> {
+    ): Promise<V2.GetABISnapshotResponse> {
         if (!block) {
             const info = await this.client.v1.chain.get_info()
 
@@ -40,7 +65,7 @@ export class HyperionAPIClient {
                 contract
             )}&block=${block}&fetch=${fetch}`,
             method: 'GET',
-            responseType: GetABISnapshotResponse,
+            responseType: V2.GetABISnapshotResponse,
         })
     }
 
@@ -49,7 +74,7 @@ export class HyperionAPIClient {
         proxy?: boolean,
         skip?: number,
         limit?: number
-    ): Promise<GetVotersResponse> {
+    ): Promise<V2.GetVotersResponse> {
         let queryParams = ''
         const queryParts: string[] = []
 
@@ -63,17 +88,17 @@ export class HyperionAPIClient {
         return this.client.call({
             path: `/v2/state/get_voters${queryParams}`,
             method: 'GET',
-            responseType: GetVotersResponse,
+            responseType: V2.GetVotersResponse,
         })
     }
 
-    async get_links(account?: NameType): Promise<GetLinksResponse> {
+    async get_links(account?: NameType): Promise<V2.GetLinksResponse> {
         const queryParams = account ? `?account=${account}` : ''
 
         return this.client.call({
             path: `/v2/state/get_links${queryParams}`,
             method: 'GET',
-            responseType: GetLinksResponse,
+            responseType: V2.GetLinksResponse,
         })
     }
 
@@ -86,7 +111,7 @@ export class HyperionAPIClient {
         track?: number | boolean
         skip?: number
         limit?: number
-    }): Promise<GetProposalsResponse> {
+    }): Promise<V2.GetProposalsResponse> {
         const queryParts: string[] = []
 
         for (const [key, value] of Object.entries(options || {})) {
@@ -98,7 +123,7 @@ export class HyperionAPIClient {
         return this.client.call({
             path: `/v2/state/get_proposals${queryParams}`,
             method: 'GET',
-            responseType: GetProposalsResponse,
+            responseType: V2.GetProposalsResponse,
         })
     }
 
@@ -117,7 +142,7 @@ export class HyperionAPIClient {
             act_name?: string
             act_account?: NameType
         }
-    ): Promise<GetActionsResponse> {
+    ): Promise<V2.GetActionsResponse> {
         const queryParts: string[] = [`account=${account}`]
 
         for (const [key, value] of Object.entries(options || {})) {
@@ -129,23 +154,23 @@ export class HyperionAPIClient {
         return this.client.call({
             path: `/v2/history/get_actions${queryParams}`,
             method: 'GET',
-            responseType: GetActionsResponse,
+            responseType: V2.GetActionsResponse,
         })
     }
 
-    async get_created_accounts(account: NameType): Promise<GetCreatedAccountsResponse> {
+    async get_created_accounts(account: NameType): Promise<V2.GetCreatedAccountsResponse> {
         return this.client.call({
             path: `/v2/history/get_created_accounts?account=${account}`,
             method: 'GET',
-            responseType: GetCreatedAccountsResponse,
+            responseType: V2.GetCreatedAccountsResponse,
         })
     }
 
-    async get_creator(account: NameType): Promise<GetCreatorResponse> {
+    async get_creator(account: NameType): Promise<V2.GetCreatorResponse> {
         return this.client.call({
             path: `/v2/history/get_creator?account=${account}`,
             method: 'GET',
-            responseType: GetCreatorResponse,
+            responseType: V2.GetCreatorResponse,
         })
     }
 
@@ -154,11 +179,11 @@ export class HyperionAPIClient {
         scope: NameType,
         table: NameType,
         payer: NameType
-    ): Promise<GetDeltasResponse> {
+    ): Promise<V2.GetDeltasResponse> {
         return this.client.call({
             path: `/v2/history/get_deltas?code=${code}&scope=${scope}&table=${table}&payer=${payer}`,
             method: 'GET',
-            responseType: GetDeltasResponse,
+            responseType: V2.GetDeltasResponse,
         })
     }
 
@@ -167,35 +192,35 @@ export class HyperionAPIClient {
         table: NameType,
         block_num: Int64Type,
         after_key = ''
-    ): Promise<GetTableStateResponse> {
+    ): Promise<V2.GetTableStateResponse> {
         return this.client.call({
             path: `/v2/history/get_table_state?code=${code}&table=${table}&block_num=${block_num}&after_key=${after_key}`,
             method: 'GET',
-            responseType: GetTableStateResponse,
+            responseType: V2.GetTableStateResponse,
         })
     }
 
-    async get_key_accounts(public_key: PublicKeyType): Promise<GetKeyAccountsResponse> {
+    async get_key_accounts(public_key: PublicKeyType): Promise<V2.GetKeyAccountsResponse> {
         return this.client.call({
             path: `/v2/state/get_key_accounts?public_key=${public_key}`,
             method: 'GET',
-            responseType: GetKeyAccountsResponse,
+            responseType: V2.GetKeyAccountsResponse,
         })
     }
 
-    async get_tokens(account: NameType): Promise<GetTokensResponse> {
+    async get_tokens(account: NameType): Promise<V2.GetTokensResponse> {
         return this.client.call({
             path: `/v2/state/get_tokens?account=${account}`,
             method: 'GET',
-            responseType: GetTokensResponse,
+            responseType: V2.GetTokensResponse,
         })
     }
 
-    async get_transaction(id: Checksum256Type): Promise<GetTransactionResponse> {
+    async get_transaction(id: Checksum256Type): Promise<V2.GetTransactionResponse> {
         return this.client.call({
             path: `/v2/history/get_transaction?id=${id}`,
             method: 'GET',
-            responseType: GetTransactionResponse,
+            responseType: V2.GetTransactionResponse,
         })
     }
 }
--- test/api.ts
+++ test/api.ts
@@ -19,102 +19,126 @@ suite('Hyperion API', function () {
     this.slow(200)
     this.timeout(10 * 10000)
 
-    test('get_abi_snapshot', async function () {
-        const response = await hyperion.get_abi_snapshot("eosio.token", 2000, true);
-        assert.equal(response.abi.version, "eosio::abi/1.1")
+    suite('v1', function () {
+        test('get_actions', async function () {
+            const response = await hyperion.v1.get_actions({
+                account_name: "teamgreymass",
+                pos: 100,
+                offset: 5,
+                filter: 1,
+                sort: "asc",
+                after: 100,
+                before: 200,
+                parent: 1
+            });
+
+            console.log({ json: JSON.stringify(response) })
+
+            assert.isArray(response.actions);
+            assert.equal(response.actions.length, 5);
+            assert.instanceOf(response.actions[0].act.name, Name);
+            assert.instanceOf(response.actions[0].act.authorization[0].actor, Name);
+        })
     })
 
-    test('get_voters', async function () {
-        const response = await hyperion.get_voters('teamgreymass', true, 100, 200);
-        assert.equal(response.voters.length, 100);
-        assert.instanceOf(response.voters[0].account, Name);
-        assert.equal(String(response.voters[0].account), 'j3.rq.wam');
-        assert.equal(Number(response.voters[0].weight), 1.5044807131168136e+39);
-        assert.equal(Number(response.voters[0].last_vote), 266448316);
-    });
-
-    test('get_links', async function () {
-        const response = await hyperion.get_links('teamgreymass');
-        assert.isArray(response.links);
-        assert.equal(response.links.length, 10);
-        assert.instanceOf(response.links[0].account, Name);
-    });
-
-    test('get_proposals', async function () {
-        const response = await hyperion.get_proposals({
-            skip: 1,
-            limit: 10
-        });
-
-        assert.isArray(response.proposals);
-        assert.equal(response.proposals.length, 10);
-        assert.instanceOf(response.proposals[0].proposer, Name);
-    })
-
-    test('get_actions', async function () {
-        const response = await hyperion.get_actions("teamgreymass", {
-            filter: "eosio.token:*",
-            skip: 100,
-            limit: 5
-        });
-
+    suite('v2', function () {
+        test('get_abi_snapshot', async function () {
+            const response = await hyperion.v2.get_abi_snapshot("eosio.token", 2000, true);
+            assert.equal(response.abi.version, "eosio::abi/1.1")
+        })
     
-        assert.isArray(response.actions);
-        assert.equal(response.actions.length, 5);
-        assert.instanceOf(response.actions[0].act.name, Name);
-        assert.instanceOf(response.actions[0].act.authorization[0].actor, Name);
-    });
-
-    test('get_created_accounts', async function () {
-        const response = await hyperion.get_created_accounts("teamgreymass");
-
-        assert.isArray(response.accounts);
-        assert.equal(response.accounts.length, 4);
-        assert.instanceOf(response.accounts[0].name, Name);
-    })
-
-    test('get_creator', async function () {
-        const response = await hyperion.get_creator("teamgreymass");
-
-        assert.instanceOf(response.creator, Name);
-        assert.equal(String(response.creator), "gqyqi.waa");
-    })
-
-    test('get_deltas', async function () {
-        const response = await hyperion.get_deltas("eosio.token", "teamgreymass", "accounts", "teamgreymass");
-
-        assert.equal(response.deltas.length, 10);
-        assert.instanceOf(response.deltas[0].code, Name);
-    })
-
-    test('get_table_state', async function () {
-        const response = await hyperion.get_table_state("eosio.token", "stat", 267000000);
-
-        assert.instanceOf(response.code, Name);
-        assert.equal(response.results.length, 1);
-    })
-
-    test('get_key_accounts', async function () {
-        const response = await hyperion.get_key_accounts("EOS8KmhygTrrvtW7zJd6HXWrNqA5WX9NzScZ37JyXRiwpiJN2g2rR");
+        test('get_voters', async function () {
+            const response = await hyperion.v2.get_voters('teamgreymass', true, 100, 200);
+            assert.equal(response.voters.length, 100);
+            assert.instanceOf(response.voters[0].account, Name);
+            assert.equal(String(response.voters[0].account), 'j3.rq.wam');
+            assert.equal(Number(response.voters[0].weight), 1.5044807131168136e+39);
+            assert.equal(Number(response.voters[0].last_vote), 266448316);
+        });
+    
+        test('get_links', async function () {
+            const response = await hyperion.v2.get_links('teamgreymass');
+            assert.isArray(response.links);
+            assert.equal(response.links.length, 10);
+            assert.instanceOf(response.links[0].account, Name);
+        });
+    
+        test('get_proposals', async function () {
+            const response = await hyperion.v2.get_proposals({
+                skip: 1,
+                limit: 10
+            });
+    
+            assert.isArray(response.proposals);
+            assert.equal(response.proposals.length, 10);
+            assert.instanceOf(response.proposals[0].proposer, Name);
+        })
+    
+        test('get_actions', async function () {
+            const response = await hyperion.v2.get_actions("teamgreymass", {
+                filter: "eosio.token:*",
+                skip: 100,
+                limit: 5
+            });
+    
         
-        assert.equal(response.account_names.length, 1);
-        assert.instanceOf(response.account_names[0], Name);
-        assert.equal(String(response.account_names[0]), "teamgreymass");
-    })
-
-    test('get_tokens', async function () {
-        const response = await hyperion.get_tokens("teamgreymass");
-
-        assert.instanceOf(response.account, Name);
-        assert.equal(response.tokens.length, 2);
-        assert.instanceOf(response.tokens[0].amount, Float64);
-    })
-
-    test('get_transaction', async function () {
-        const response = await hyperion.get_transaction("a51a3cc53b2ff5d5b25ad44b1e3ef5f796ce3ca60101ea05b3be64e68b684ccb");
-
-        assert.instanceOf(response.trx_id, Checksum256);
-        assert.equal(response.actions.length, 5);
-        assert.instanceOf(response.actions[0].act.name, Name);
-    })
+            assert.isArray(response.actions);
+            assert.equal(response.actions.length, 5);
+            assert.instanceOf(response.actions[0].act.name, Name);
+            assert.instanceOf(response.actions[0].act.authorization[0].actor, Name);
+        });
+    
+        test('get_created_accounts', async function () {
+            const response = await hyperion.v2.get_created_accounts("teamgreymass");
+    
+            assert.isArray(response.accounts);
+            assert.equal(response.accounts.length, 4);
+            assert.instanceOf(response.accounts[0].name, Name);
+        })
+    
+        test('get_creator', async function () {
+            const response = await hyperion.v2.get_creator("teamgreymass");
+    
+            assert.instanceOf(response.creator, Name);
+            assert.equal(String(response.creator), "gqyqi.waa");
+        })
+    
+        test('get_deltas', async function () {
+            const response = await hyperion.v2.get_deltas("eosio.token", "teamgreymass", "accounts", "teamgreymass");
+    
+            assert.equal(response.deltas.length, 10);
+            assert.instanceOf(response.deltas[0].code, Name);
+        })
+    
+        test('get_table_state', async function () {
+            const response = await hyperion.v2.get_table_state("eosio.token", "stat", 267000000);
+    
+            assert.instanceOf(response.code, Name);
+            assert.equal(response.results.length, 1);
+        })
+    
+        test('get_key_accounts', async function () {
+            const response = await hyperion.v2.get_key_accounts("EOS8KmhygTrrvtW7zJd6HXWrNqA5WX9NzScZ37JyXRiwpiJN2g2rR");
+            
+            assert.equal(response.account_names.length, 1);
+            assert.instanceOf(response.account_names[0], Name);
+            assert.equal(String(response.account_names[0]), "teamgreymass");
+        })
+    
+        test('get_tokens', async function () {
+            const response = await hyperion.v2.get_tokens("teamgreymass");
+    
+            assert.instanceOf(response.account, Name);
+            assert.equal(response.tokens.length, 2);
+            assert.instanceOf(response.tokens[0].amount, Float64);
+        })
+    
+        test('get_transaction', async function () {
+            const response = await hyperion.v2.get_transaction("a51a3cc53b2ff5d5b25ad44b1e3ef5f796ce3ca60101ea05b3be64e68b684ccb");
+    
+            assert.instanceOf(response.trx_id, Checksum256);
+            assert.equal(response.actions.length, 5);
+            assert.instanceOf(response.actions[0].act.name, Name);
+        })
+    })  
 })
